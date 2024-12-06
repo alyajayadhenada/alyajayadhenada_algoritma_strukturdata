5e859434bc8af1f6aadc44cb14f294bb1ed79d8c
alyajayadhenada <alyadhenada@gmail.com>
2024-12-06 09:54:14 +0700

    Add files via upload

---
+++ list kota.py
@@ -0,0 +1,10 @@
+listkota = [
+    'jakarta', 'surabaya', 'depok', 'bekasi', 'solo', 'jogjakarta', 'semarang', 'pangkalpinang', 
+]
+
+
+#Bermain index
+i = 0
+while i < len(listkota):
+    print(listkota[i])
+    i += 1
