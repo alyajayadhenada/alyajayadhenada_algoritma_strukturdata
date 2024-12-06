2c5ef3f8baabe676d02f4127ea89722a78da3367
alyajayadhenada <alyadhenada@gmail.com>
2024-12-06 10:07:16 +0700

    Add files via upload

--- list kota.py
+++ list kota.py
@@ -2,9 +2,14 @@ listkota = [
     'jakarta', 'surabaya', 'depok', 'bekasi', 'solo', 'jogjakarta', 'semarang', 'pangkalpinang', 
 ]
 
-
-#Bermain index
+kotaYangDicari = input('masukkan nama kota yang dicari: ')
+ 
 i = 0
 while i < len(listkota):
-    print(listkota[i])
+    if listkota [i].lower() == kotaYangDicari.lower():
+        print('Ketemu di index', i)
+        break
+    print('Bukan ', listkota[i])
     i += 1
+else:
+    print('Maaf, kota yang anda cari tidak ditemukan. ')
