da6c10a1a41f8bd52b97a9135e51ca16f688bfdd
alyajayadhenada <alyadhenada@gmail.com>
2024-12-21 20:03:38 +0700

    Add files via upload

---
+++ biodata.py
@@ -0,0 +1,12 @@
+nama_depan = "alya"
+nama_belakang = 'jaya dhenada'
+nama_lengkap = nama_depan + '' + nama_belakang
+usia = '18'
+alamat = "jebus"
+kata_mutiara = "hidup adalah algoritma yang terus di optimalkan setiap langkah membawa kita lebih dekaat pada solusi terbaik"
+
+print (nama_lengkap, '('+ usia +')',',','dari', alamat,',kata mutiara:', kata_mutiara)
+
+print('\ntipe dari nama_lengkap:', type(nama_lengkap), 'tipe dari usia:',type (usia))
+print('tipe dari alamat:', type(alamat))
+print('tipe dari kata_mutiara:', type(kata_mutiara))
