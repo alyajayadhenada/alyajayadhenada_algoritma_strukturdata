e20d89090ac7debf05c4adec0367c0fcbb9c7bdd
alyajayadhenada <alyadhenada@gmail.com>
2024-12-21 20:21:37 +0700

    Add files via upload

---
+++ # List memasukkan angka 5.py
@@ -0,0 +1,13 @@
+# List memasukkan angka 5
+list_angka = []
+
+# list memasukkan angka 5
+list_angka = ['1','2','3','4','5']
+
+print(list_angka[0])
+print(list_angka[1])
+print(list_angka[2])
+print(list_angka[3])
+print(list_angka[4])
+print(list_angka[5])
+
